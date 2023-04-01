c651a619989705cb5e146b1a4b8c9704d608b46d
santosh <sivasantoshbhogi@gmail.com>
2023-04-01 14:34:01 +0530

    webapp is ready

--- app.py
+++ app.py
@@ -17,9 +17,18 @@ def predict_api():
     data=request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    new_data=model.transform(np.array(list(data.values())).reshape(1,-1))
     output=model.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+     data=[float(x) for x in request.form.values()]
+     final_input=scalar.transform(np.array(data).reshape(1,-1))
+     print(final_input)
+     output=model.predict(final_input)[0]
+     return render_template("home.html",prediction_text="The predicted Load is {}".format(output))
+
 if __name__=="__main__":
     app.run(debug=True)
